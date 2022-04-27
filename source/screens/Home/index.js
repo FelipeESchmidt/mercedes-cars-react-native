e62e7068bb618723efa64eedc7c15a51fb4caa3b
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-27 19:26:55 -0300

    feat: developing home screen

---
+++ source/screens/Home/index.js
@@ -0,0 +1,45 @@
+import { View, FlatList } from "react-native";
+
+import mercedes from "../../mocks/mercedes";
+
+import Car from "../../components/Car";
+import Header from "../../components/Header";
+import OSText from "../../components/OSText";
+
+import { styles } from "./index.styles";
+import Footer from "../../components/Footer";
+
+function HomeScreen() {
+  return (
+    <>
+      <FlatList
+        data={mercedes.cars.available}
+        renderItem={Car}
+        keyExtractor={({ name }) => name}
+        ListHeaderComponent={
+          <>
+            <Header {...mercedes.header} />
+            <OSText style={styles.text}>
+              {`${mercedes.cars.availableTitle} (${mercedes.cars.available.length})`}
+            </OSText>
+          </>
+        }
+        ListFooterComponent={
+          <FlatList
+            data={mercedes.cars.unavailable}
+            renderItem={Car}
+            keyExtractor={({ name }) => name}
+            ListHeaderComponent={
+              <OSText style={styles.text}>
+                {`${mercedes.cars.unavailableTitle} (${mercedes.cars.unavailable.length})`}
+              </OSText>
+            }
+            ListFooterComponent={<Footer {...mercedes.footer} />}
+          />
+        }
+      />
+    </>
+  );
+}
+
+export default HomeScreen;
---
+++ source/screens/Home/index.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  text: {
+    margin: 20,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "red",
+    color: "#b3b3b3",
+  },
+});
